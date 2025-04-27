a317008177ea7066dbfd259cfe5d9bfde1692fb4
Chris jr Nwaije <okem.nwaije@gmail.com>
2025-04-27 06:52:42 +0000

    Refactor database population to use PyMongo and drop existing collections before inserting test data

--- octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
+++ octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
@@ -1,45 +1,43 @@
 from django.core.management.base import BaseCommand
+from django.conf import settings
 from octofit_tracker.test_data import TEST_USERS, TEST_TEAMS, TEST_ACTIVITIES, TEST_LEADERBOARD, TEST_WORKOUTS
-from octofit_tracker.models import User, Team, Activity, Leaderboard, Workout
-from bson import ObjectId
+from pymongo import MongoClient
 from datetime import timedelta
 
 class Command(BaseCommand):
-    help = 'Populate the octofit_db database with test data.'
+    help = 'Populate the octofit_db database with test data using PyMongo.'
 
     def handle(self, *args, **kwargs):
-        # Clear existing data
-        User.objects.all().delete()
-        Team.objects.all().delete()
-        Activity.objects.all().delete()
-        Leaderboard.objects.all().delete()
-        Workout.objects.all().delete()
+        # Connect to MongoDB
+        client = MongoClient(settings.DATABASES['default']['HOST'], settings.DATABASES['default']['PORT'])
+        db = client[settings.DATABASES['default']['NAME']]
 
-        # Create users
-        user_objs = {}
-        for user in TEST_USERS:
-            obj = User.objects.create(_id=ObjectId(), username=user['username'], email=user['email'], password=user['password'])
-            user_objs[user['username']] = obj
-        self.stdout.write(self.style.SUCCESS('Users created.'))
+        # Drop existing collections
+        db.users.drop()
+        db.teams.drop()
+        db.activities.drop()
+        db.leaderboard.drop()
+        db.workouts.drop()
 
-        # Create teams
+        # Insert users
+        db.users.insert_many(TEST_USERS)
+
+        # Assign user ObjectIds to teams, activities, leaderboard
+        user_ids = [user['_id'] for user in TEST_USERS]
         for team in TEST_TEAMS:
-            team_obj = Team.objects.create(_id=ObjectId(), name=team['name'])
-            for member in team['members']:
-                team_obj.members.add(user_objs[member])
-        self.stdout.write(self.style.SUCCESS('Teams created.'))
+            team['members'] = user_ids
+        db.teams.insert_many(TEST_TEAMS)
 
-        # Create activities
-        for activity in TEST_ACTIVITIES:
-            Activity.objects.create(_id=ObjectId(), user=user_objs[activity['user']], activity_type=activity['activity_type'], duration=timedelta(seconds=activity['duration']))
-        self.stdout.write(self.style.SUCCESS('Activities created.'))
+        # Assign users to activities and leaderboard
+        for i, activity in enumerate(TEST_ACTIVITIES):
+            activity['user'] = user_ids[i]
+            if isinstance(activity['duration'], timedelta):
+                activity['duration'] = int(activity['duration'].total_seconds())
+        db.activities.insert_many(TEST_ACTIVITIES)
 
-        # Create leaderboard
-        for entry in TEST_LEADERBOARD:
-            Leaderboard.objects.create(_id=ObjectId(), user=user_objs[entry['user']], score=entry['score'])
-        self.stdout.write(self.style.SUCCESS('Leaderboard created.'))
+        for i, entry in enumerate(TEST_LEADERBOARD):
+            entry['user'] = user_ids[i]
+        db.leaderboard.insert_many(TEST_LEADERBOARD)
 
-        # Create workouts
-        for workout in TEST_WORKOUTS:
-            Workout.objects.create(_id=ObjectId(), name=workout['name'], description=workout['description'])
-        self.stdout.write(self.style.SUCCESS('Workouts created.'))
+        db.workouts.insert_many(TEST_WORKOUTS)
+        self.stdout.write(self.style.SUCCESS('Successfully populated octofit_db with test data using PyMongo.'))
